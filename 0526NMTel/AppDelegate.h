ede044c110997da98f1045802dbe6b0976a4c67e
Zhixue Li <schcat@Zhixues-MacBook-Pro.local>
2015-05-26 19:50:15 +0800

    Initial Commit

---
+++ 0526NMTel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0526NMTel
+//
+//  Created by Zhixue Li on 5/26/15.
+//  Copyright (c) 2015 Zhixue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
